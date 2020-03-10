a851e57db6c8fbf6c4dff9c6d3127507c2bf915a
williewong68 <williewong68@gmail.com>
2020-03-10 16:31:11 +0800

    modify Prj1/HelloWorld2.java

--- Prj1/src/com/xxx/HelloWorld2.java
+++ Prj1/src/com/xxx/HelloWorld2.java
@@ -3,7 +3,7 @@ package com.xxx;
 public class HelloWorld2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 2");
     }
 
 }
